a9ae34138071df827cb05e2571675486f2800d20
giselleLago <gisellelago25994@gmail.com>
2020-08-01 17:32:59 +0200

    Add project files.

---
+++ CW_FindTheParityOutlier/Kata.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CW_FindTheParityOutlier
+{
+    public class Kata
+    {
+        public int Find(int[] integers)
+        {
+            var countP = new List<int>();
+            var countI = new List<int>();
+            foreach (var item in integers)
+            {
+                if (item % 2 == 0)
+                {
+                    countP.Add(item);
+                }
+                else
+                {
+                    countI.Add(item);
+                }
+            }
+
+            if (countP.Count() == 1)
+            {
+                foreach (var item in countP)
+                {
+                    return item;
+                }
+            }
+            else
+            {
+                foreach (var item in countI)
+                {
+                    return item;
+                }
+            }
+            return -1;
+            //-------------------------------------------------------------------------
+            //var evenNumbers = integers.Where(integer => integer % 2 == 0);
+            //var oddNumbers = integers.Where(integer => integer % 2 == 1);
+            //return evenNumbers.Count() == 1 ? evenNumbers.First() : oddNumbers.First();
+        }
+    }
+}
---
+++ CW_FindTheParityOutlier/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CW_FindTheParityOutlier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] exampleTest1 = { 160, 3, 1719, 19, 11, 13, -21 };
+            Console.WriteLine("Hello World!");
+            var a = new Kata();
+            Console.WriteLine(a.Find(exampleTest1));
+            Console.ReadKey();
+        }
+    }
+}
